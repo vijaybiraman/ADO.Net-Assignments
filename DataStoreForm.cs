04c5b876f109a1f5f7431ae0107fa8533fb8d6ec
vijaybiraman <109381804+vijaybiraman@users.noreply.github.com>
2022-08-03 08:31:08 +0530

    Add files via upload

---
+++ DataStoreForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace ClassLibDemo
+{
+    public class DataStoreForm
+    {
+        SqlConnection connection = null;
+        SqlCommand command = null;
+        SqlDataReader reader = null;
+
+        public DataStoreForm(string connectionString)
+        {
+            connection = new SqlConnection(connectionString);
+        }
+        public List<Login> ValidateUser()
+        {
+            try
+            {
+                
+                string sql = "Select Username,password from UserData";
+                command = new SqlCommand(sql, connection);
+              
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                reader = command.ExecuteReader();
+                List<Login> users = new List<Login>();
+                while (reader.Read())
+                {
+                    Login login = new Login();
+                    login.username= reader["UserName"].ToString();
+                    login.password = reader["Password"].ToString();
+                   
+                    users.Add(login);
+                }
+
+                return users;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            
+        }
+    }
+}
+
+
+    
---
+++ LoginPage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Configuration;
+using ClassLibDemo;
+namespace WindowsFormsApp1
+{
+    public partial class LoginPage : Form
+    {
+       
+        DataStoreForm dataStoreForm = null;
+     
+        public LoginPage()
+        {
+            InitializeComponent();
+            dataStoreForm = new DataStoreForm(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+           
+            List<Login> login = dataStoreForm.ValidateUser();
+            bool res = false;
+            foreach(var i in login)
+            {
+                if(i.password==txtPassword.Text && i.username==txtUsername.Text)
+                {
+                    res = true;
+                   WinformDB db=new WinformDB();
+                    db.ShowDialog();
+                }
+            }
+            if (res == false)
+            {
+                MessageBox.Show("Invalid user");
+            }
+        }
+    }
+}
